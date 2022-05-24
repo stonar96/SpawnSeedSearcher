05b028ded093bab6ef183fca9c71bd9940af4a40
stonar96 <minecraft.stonar96@gmail.com>
2022-05-24 23:30:20 +0200

    Add set relation id option and ability to define multiple biome id sets

--- main.c
+++ main.c
@@ -11,9 +11,28 @@ union Rand64 {
     uint8_t values[8];
 };
 
+typedef struct BiomeSequenceData BiomeSequenceData;
+
+struct BiomeSequenceData {
+    Generator* g;
+    int x;
+    int z;
+    int radius;
+    int biome;
+    int biomeCalculated;
+};
+
+enum SetRelation {
+    EQUAL, SUBSET, SUPERSET, INTERSECT
+};
+
 uint64_t rand64(void);
 
-int checkBiome(const Generator*, int, int, int, int, int, int[], int*);
+int biomeSequence(int, void*);
+
+int setBySequenceIndicatorFunction(int, int(*)(int, void*), void*, int, int[], int*[], int);
+
+int indexOf(int, int, int[]);
 
 char* getLine(char*, int);
 
@@ -92,53 +111,100 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    char* biomesString = line1;
+    char* setRelationString = line1;
 
     if (argc >= 6) {
-        biomesString = argv[5];
+        setRelationString = argv[5];
     } else {
-        printf("Enter biome ids separated by ',' or press enter to use all biome ids: ");
+        printf("Enter set relation id or press enter to use equal: ");
+        getLine(setRelationString, n);
+    }
+
+    int setRelation = EQUAL;
+
+    if (setRelationString[0] != '\0' && (sscanf(setRelationString, "%d", &setRelation) != 1 || setRelation < 0 || setRelation > 3)) {
+        printf("Invalid set relation id '%s'.\n", setRelationString);
+        getchar();
+        return 1;
+    }
+
+    char* biomesString = line1;
+
+    if (argc >= 7) {
+        biomesString = argv[6];
+    } else {
+        printf("Enter biome id sets separated by '|' with biome ids separated by ',' or press enter to use no biome id sets: ");
         getLine(biomesString, n);
     }
 
-    int biomeCount = 0;
+    int biomeSetCount = 0;
 
     if (biomesString[0] != '\0') {
-        biomeCount = 1;
+        biomeSetCount = 1;
 
         for (int i = 0; biomesString[i] != '\0'; i++) {
-            if (biomesString[i] == ',') {
-                biomeCount++;
+            if (biomesString[i] == '|') {
+                biomeSetCount++;
             }
         }
     }
 
-    int biomes[biomeCount];
+    int biomeCounts[biomeSetCount];
+    int* biomeSets[biomeSetCount];
 
-    for (int i = 0, j = 0; i < biomeCount; i++) {
-        int k = 0;
+    for (int i = 0, j = 0; i < biomeSetCount; i++) {
+        int biomeCount = 0;
 
-        for (; biomesString[j] != '\0' && biomesString[j] != ','; j++, k++) {
-            line2[k] = biomesString[j];
+        if (biomesString[j] != '|' && biomesString[j] != '\0') {
+            biomeCount = 1;
+
+            for (int k = j; biomesString[k] != '|' && biomesString[k] != '\0'; k++) {
+                if (biomesString[k] == ',') {
+                    biomeCount++;
+                }
+            }
         }
 
-        line2[k] = '\0';
-        j++;
-        int biome;
+        int* biomes = malloc(biomeCount * sizeof(int));
 
-        if (sscanf(line2, "%d", &biome) != 1) {
-            printf("Invalid biome id '%s'.\n", line2);
-            getchar();
-            return 1;
+        for (int k = 0; k < biomeCount; k++) {
+            int l = 0;
+
+            for (; biomesString[j] != '|' && biomesString[j] != '\0' && biomesString[j] != ','; j++, l++) {
+                line2[l] = biomesString[j];
+            }
+
+            line2[l] = '\0';
+            j++;
+            int biome;
+
+            if (sscanf(line2, "%d", &biome) != 1) {
+                printf("Invalid biome id '%s'.\n", line2);
+                getchar();
+                return 1;
+            }
+
+            if (indexOf(biome, k, biomes) != -1) {
+                printf("Duplicate biome id '%s'.\n", line2);
+                getchar();
+                return 1;
+            }
+
+            biomes[k] = biome;
         }
 
-        biomes[i] = biome;
+        if (biomeCount == 0) {
+            j++;
+        }
+
+        biomeCounts[i] = biomeCount;
+        biomeSets[i] = biomes;
     }
 
     char* spawnString = line1;
 
-    if (argc >= 7) {
-        spawnString = argv[6];
+    if (argc >= 8) {
+        spawnString = argv[7];
     } else {
         printf("Enter 'y' to check if the spawn is at the given coordinates or press enter: ");
         getLine(spawnString, n);
@@ -154,8 +220,8 @@ int main(int argc, char* argv[]) {
 
     char* dimString = line1;
 
-    if (argc >= 8) {
-        dimString = argv[7];
+    if (argc >= 9) {
+        dimString = argv[8];
     } else {
         printf("Enter dimension id or press enter to use the default dimension id: ");
         getLine(dimString, n);
@@ -171,8 +237,8 @@ int main(int argc, char* argv[]) {
 
     char* flagsString = line1;
 
-    if (argc >= 9) {
-        flagsString = argv[8];
+    if (argc >= 10) {
+        flagsString = argv[9];
     } else {
         printf("Enter flags or press enter to use the default flags: ");
         getLine(flagsString, n);
@@ -196,9 +262,9 @@ int main(int argc, char* argv[]) {
         applySeed(&g, dim, seed);
         Pos pos;
         int posCalculated = 0;
-        int biome;
+        int biome = -1;
         int biomeCalculated = 0;
-        int biomesCheck = biomeCount == 0;
+        int biomesCheck = biomeSetCount == 0;
 
         if (!biomesCheck) {
             if (xSpawn || zSpawn) {
@@ -214,32 +280,16 @@ int main(int argc, char* argv[]) {
                 }
             }
 
-            if (checkBiome(&g, 1, x, 64, z, biomeCount, biomes, &biome)) {
-                if (radius == 0) {
-                    biomesCheck = 1;
-                } else if (checkBiome(&g, 1, x - radius, 64, z - radius, biomeCount, biomes, NULL) && checkBiome(&g, 1, x - radius, 64, z + radius, biomeCount, biomes, NULL) && checkBiome(&g, 1, x + radius, 64, z - radius, biomeCount, biomes, NULL) && checkBiome(&g, 1, x + radius, 64, z + radius, biomeCount, biomes, NULL)) {
-                    biomesCheck = 1;
-
-                    for (int i = -radius; i <= radius; i++) {
-                        for (int j = -radius; j <= radius; j++) {
-                            if (i == 0 && j == 0 || (i == -radius || i == radius) && (j == -radius || j == radius)) {
-                                continue;
-                            }
-
-                            if (!checkBiome(&g, 1, x + i, 64, z + j, biomeCount, biomes, NULL)) {
-                                biomesCheck = 0;
-                                break;
-                            }
-                        }
-
-                        if (!biomesCheck) {
-                            break;
-                        }
-                    }
-                }
-            }
-
-            biomeCalculated = 1;
+            BiomeSequenceData biomeSequenceData;
+            biomeSequenceData.g = &g;
+            biomeSequenceData.x = x;
+            biomeSequenceData.z = z;
+            biomeSequenceData.radius = radius;
+            biomeSequenceData.biome = biome;
+            biomeSequenceData.biomeCalculated = biomeCalculated;
+            biomesCheck = setBySequenceIndicatorFunction((2 * radius + 1) * (2 * radius + 1), biomeSequence, &biomeSequenceData, biomeSetCount, biomeCounts, biomeSets, setRelation);
+            biome = biomeSequenceData.biome;
+            biomeCalculated = biomeSequenceData.biomeCalculated;
         }
 
         if (biomesCheck) {
@@ -307,20 +357,296 @@ uint64_t rand64(void) {
     return result.value;
 }
 
-int checkBiome(const Generator* g, int scale, int x, int y, int z, int biomeCount, int biomes[], int* biome) {
-    int b = getBiomeAt(g, scale, x, y, z);
+int biomeSequence(int index, void* currying) {
+    BiomeSequenceData* biomeSequenceData = currying;
 
-    if (biome != NULL) {
-        *biome = b;
+    if (biomeSequenceData->radius < 0) {
+        return -1;
     }
 
-    for (int i = 0; i < biomeCount; i++) {
-        if (biomes[i] == b) {
+    if (index == 0) {
+        int biome = getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x, 64, biomeSequenceData->z);
+        biomeSequenceData->biome = biome;
+        biomeSequenceData->biomeCalculated = 1;
+        return biome;
+    }
+
+    if (biomeSequenceData->radius == 0) {
+        return -1;
+    }
+
+    if (index == 1) {
+        return getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x - biomeSequenceData->radius, 64, biomeSequenceData->z - biomeSequenceData->radius);
+    }
+
+    if (index == 2) {
+        return getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x - biomeSequenceData->radius, 64, biomeSequenceData->z + biomeSequenceData->radius);
+    }
+
+    if (index == 3) {
+        return getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x + biomeSequenceData->radius, 64, biomeSequenceData->z - biomeSequenceData->radius);
+    }
+
+    if (index == 4) {
+        return getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x + biomeSequenceData->radius, 64, biomeSequenceData->z + biomeSequenceData->radius);
+    }
+
+    index -= 5;
+
+    if (index >= 0) {
+        index++;
+
+        if (index >= 2 * biomeSequenceData->radius) {
+            index++;
+
+            if (index >= 2 * (biomeSequenceData->radius * biomeSequenceData->radius + biomeSequenceData->radius)) {
+                index++;
+
+                if (index >= 2 * (2 * biomeSequenceData->radius * biomeSequenceData->radius + biomeSequenceData->radius)) {
+                    index++;
+
+                    if (index >= 4 * (biomeSequenceData->radius * biomeSequenceData->radius + biomeSequenceData->radius)) {
+                        return -1;
+                    }
+                }
+            }
+        }
+    } else {
+        return -1;
+    }
+
+    int i = index / (2 * biomeSequenceData->radius + 1);
+    int j = index - i * (2 * biomeSequenceData->radius + 1);
+    return getBiomeAt(biomeSequenceData->g, 1, biomeSequenceData->x - biomeSequenceData->radius + i, 64, biomeSequenceData->z - biomeSequenceData->radius + j);
+}
+
+int setBySequenceIndicatorFunction(int sequenceLength, int(* sequence)(int, void*), void* currying, int setCount, int elementCounts[], int* sets[], int setRelation) {
+    if (setRelation == EQUAL) {
+        if (sequenceLength <= 0) {
+            // Start optimization.
+            if (setCount <= 0) {
+                return 0;
+            }
+            // End optimization.
+
+            // Start optimization.
+            for (int i = 0; i < setCount; i++) {
+                if (elementCounts[i] <= 0) {
+                    return 1;
+                }
+            }
+            // End optimization.
+        } else {
+            // Start optimization.
+            int empty = 1;
+
+            for (int i = 0; i < setCount; i++) {
+                if (elementCounts[i] > 0) {
+                    empty = 0;
+                    break;
+                }
+            }
+
+            if (empty) {
+                return 0;
+            }
+            // End optimization.
+        }
+
+        int subset[setCount];
+
+        for (int i = 0; i < setCount; i++) {
+            subset[i] = 1;
+        }
+
+        int* superset[setCount];
+
+        for (int i = 0; i < setCount; i++) {
+            superset[i] = calloc(elementCounts[i], sizeof(int));
+        }
+
+        for (int i = 0; i < sequenceLength; i++) {
+            int element = sequence(i, currying);
+
+            for (int j = 0; j < setCount; j++) {
+                if (subset[j]) {
+                    int index = indexOf(element, elementCounts[j], sets[j]);
+
+                    if (index == -1) {
+                        subset[j] = 0;
+
+                        if (indexOf(1, setCount, subset) == -1) {
+                            for (int k = 0; k < setCount; k++) {
+                                free(superset[k]);
+                            }
+
+                            return 0;
+                        }
+                    }
+
+                    superset[j][index] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < setCount; i++) {
+            if (subset[i] && indexOf(0, elementCounts[i], superset[i]) == -1) {
+                for (int j = 0; j < setCount; j++) {
+                    free(superset[j]);
+                }
+
+                return 1;
+            }
+        }
+
+        for (int i = 0; i < setCount; i++) {
+            free(superset[i]);
+        }
+
+        return 0;
+    } else if (setRelation == SUBSET) {
+        if (setCount <= 0) {
+            return 0;
+        }
+
+        // Start optimization.
+        if (sequenceLength <= 0) {
             return 1;
+        }
+
+        // Start optimization.
+        int empty = 1;
+
+        for (int i = 0; i < setCount; i++) {
+            if (elementCounts[i] > 0) {
+                empty = 0;
+                break;
+            }
+        }
+
+        if (empty) {
+            return 0;
+        }
+        // End optimization.
+        // End optimization.
+
+        int subset[setCount];
+
+        for (int i = 0; i < setCount; i++) {
+            subset[i] = 1;
+        }
+
+        for (int i = 0; i < sequenceLength; i++) {
+            int element = sequence(i, currying);
+
+            for (int j = 0; j < setCount; j++) {
+                if (subset[j] && indexOf(element, elementCounts[j], sets[j]) == -1) {
+                    subset[j] = 0;
+
+                    if (indexOf(1, setCount, subset) == -1) {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        return 1;
+    } else if (setRelation == SUPERSET) {
+        // Start optimization.
+        if (setCount <= 0) {
+            return 0;
+        }
+        // End optimization.
+
+        for (int i = 0; i < setCount; i++) {
+            if (elementCounts[i] <= 0) {
+                return 1;
+            }
+        }
+
+        // Start optimization.
+        if (sequenceLength <= 0) {
+            return 0;
+        }
+        // End optimization.
+
+        int* superset[setCount];
+
+        for (int i = 0; i < setCount; i++) {
+            superset[i] = calloc(elementCounts[i], sizeof(int));
+        }
+
+        for (int i = 0; i < sequenceLength; i++) {
+            int element = sequence(i, currying);
+
+            for (int j = 0; j < setCount; j++) {
+                int index = indexOf(element, elementCounts[j], sets[j]);
+
+                if (index != -1 && !superset[j][index]) {
+                    superset[j][index] = 1;
+
+                    if (indexOf(0, elementCounts[j], superset[j]) == -1) {
+                        for (int k = 0; k < setCount; k++) {
+                            free(superset[k]);
+                        }
+
+                        return 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < setCount; i++) {
+            free(superset[i]);
+        }
+
+        return 0;
+    } else if (setRelation == INTERSECT) {
+        // Start optimization.
+        // Start optimization.
+        if (sequenceLength <= 0) {
+            return 0;
+        }
+        // End optimization.
+
+        int empty = 1;
+
+        for (int i = 0; i < setCount; i++) {
+            if (elementCounts[i] > 0) {
+                empty = 0;
+                break;
+            }
+        }
+
+        if (empty) {
+            return 0;
+        }
+        // End optimization.
+
+        for (int i = 0; i < sequenceLength; i++) {
+            int element = sequence(i, currying);
+
+            for (int j = 0; j < setCount; j++) {
+                if (indexOf(element, elementCounts[j], sets[j]) != -1) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    return -1;
+}
+
+int indexOf(int value, int arrayLength, int array[]) {
+    for (int i = 0; i < arrayLength; i++) {
+        if (array[i] == value) {
+            return i;
         }
     }
 
-    return 0;
+    return -1;
 }
 
 char* getLine(char* line, int n) {
